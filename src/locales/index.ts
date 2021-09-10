111ab523fd679a39719f9dc59fe1e4f5972256ee
carolinahhernandez <carolina.hernandez@xoor.io>
2021-09-10 15:14:41 -0300

    add bn lang VIR-234

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,3 +3,4 @@ export { default as es } from './es.json';
 export { default as fr } from './fr.json';
 export { default as pt } from './pt.json';
 export { default as el } from './el.json';
+export { default as bn } from './bn.json';
