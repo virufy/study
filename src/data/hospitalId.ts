6b77d9cb51b524ae361b9d0db5a4f093af4e8e2c
carolinahhernandez <carolina.hernandez@xoor.io>
2022-02-22 10:31:21 -0300

    update hospitalIDs VIR-279

--- src/data/hospitalId.ts
+++ src/data/hospitalId.ts
@@ -1,6 +1,9 @@
 export const hospitalIdData = [
-  { value: 'IDIME-LAGO', label: 'IDIME-LAGO' },
-  { value: 'IDIME-TOBERÍN', label: 'IDIME-TOBERÍN' },
-  { value: 'IDIME-OCCIDENTE', label: 'IDIME-OCCIDENTE' },
-  { value: 'IDIME- LABMÓVIL', label: 'IDIME- LABMÓVIL' },
+  { value: 'IDIME-BOGOTA-LAGO', label: 'IDIME-BOGOTA-LAGO' },
+  { value: 'IDIME-BOGOTA-TOBERÍN', label: 'IDIME-BOGOTA-TOBERÍN' },
+  { value: 'IDIME-BOGOTA-OCCIDENTE', label: 'IDIME-BOGOTA-OCCIDENTE' },
+  { value: 'IDIME-BOGOTA- LABMÓVIL', label: 'IDIME-BOGOTA- LABMÓVIL' },
+  { value: 'IDIME-CALI-CNC', label: 'IDIME-CALI-CNC' },
+  { value: 'IDIME-CALI-CNRUU', label: 'IDIME-CALI-CNRUU' },
+  { value: 'IDIME-CALI-CDESA', label: 'IDIME-CALI-CDESA' },
 ];
