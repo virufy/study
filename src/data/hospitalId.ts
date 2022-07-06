19936040372d600c7c89663f783c8bfe17ee28bc
Maximiliano Duthey <maxi.duthey@xoor.io>
2022-07-06 18:15:27 -0300

    feat: Enable Pakistan in country list + Disabled it for Study + Enable on Clinic

--- src/data/hospitalId.ts
+++ src/data/hospitalId.ts
@@ -1,9 +1,23 @@
-export const hospitalIdData = [
-  { value: 'IDIME-BOGOTA-LAGO', label: 'IDIME-BOGOTA-LAGO' },
-  { value: 'IDIME-BOGOTA-TOBERÍN', label: 'IDIME-BOGOTA-TOBERÍN' },
-  { value: 'IDIME-BOGOTA-OCCIDENTE', label: 'IDIME-BOGOTA-OCCIDENTE' },
-  { value: 'IDIME-BOGOTA- LABMÓVIL', label: 'IDIME-BOGOTA- LABMÓVIL' },
-  { value: 'IDIME-CALI-CNC', label: 'IDIME-CALI-CNC' },
-  { value: 'IDIME-CALI-CNRUU', label: 'IDIME-CALI-CNRUU' },
-  { value: 'IDIME-CALI-CDESA', label: 'IDIME-CALI-CDESA' },
-];
+type HospitalData = {
+  value: string;
+  label: string;
+};
+
+export const hospitalIdData: CommonJSON<HospitalData[]> = {
+  Colombia: [
+    { value: 'IDIME-BOGOTA-LAGO', label: 'IDIME-BOGOTA-LAGO' },
+    { value: 'IDIME-BOGOTA-TOBERÍN', label: 'IDIME-BOGOTA-TOBERÍN' },
+    { value: 'IDIME-BOGOTA-OCCIDENTE', label: 'IDIME-BOGOTA-OCCIDENTE' },
+    { value: 'IDIME-BOGOTA- LABMÓVIL', label: 'IDIME-BOGOTA- LABMÓVIL' },
+    { value: 'IDIME-CALI-CNC', label: 'IDIME-CALI-CNC' },
+    { value: 'IDIME-CALI-CNRUU', label: 'IDIME-CALI-CNRUU' },
+    { value: 'IDIME-CALI-CDESA', label: 'IDIME-CALI-CDESA' },
+  ],
+  Pakistan: [
+    { value: 'indus-hospital', label: 'Indus Hospital' },
+  ],
+};
+
+export function getHospitalIdFor(country: string) {
+  return hospitalIdData[country] || null;
+}
