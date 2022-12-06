2f9f9a22081a10a329f2047fa9b4184ee7a16c90
ferlrodriguez <fernando.rodriguez@xoor.io>
2022-12-06 09:19:53 -0300

    fix(reportProblemForm): assign english form to japan/turkish VIR-352

--- src/data/reportProblemForm.ts
+++ src/data/reportProblemForm.ts
@@ -1,5 +1,7 @@
 export const reportProblemForm = {
   en: 'https://docs.google.com/forms/d/e/1FAIpQLSef77IOT4oxllZABL7aJlTGp6e6Q2_jPoDuIruiDxnU5Wvtxw/viewform',
+  ja: 'https://docs.google.com/forms/d/e/1FAIpQLSef77IOT4oxllZABL7aJlTGp6e6Q2_jPoDuIruiDxnU5Wvtxw/viewform',
+  tr: 'https://docs.google.com/forms/d/e/1FAIpQLSef77IOT4oxllZABL7aJlTGp6e6Q2_jPoDuIruiDxnU5Wvtxw/viewform',
   es: 'https://docs.google.com/forms/d/e/1FAIpQLScQL20FOhQbTt7lxWC3FxeynFZ4l7YmGBJJNjJdGkWuhr0dLA/viewform',
   pt: 'https://docs.google.com/forms/d/e/1FAIpQLSemk0bvHaE0IAaHNCxZjWHUBh8bALpqAoigLgLOC1VJZTQLQg/viewform',
 };
